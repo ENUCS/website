a705e33e02b986d8489fddb094f289bc264c1ae2
m <20924663+migbash@users.noreply.github.com>
2021-08-11 04:14:26 +0100

    Adding the Printful TOKEN Just in Case for Now as a tracked file

---
+++ src/config/init-printful.ts
@@ -0,0 +1,4 @@
+// exporting the printfulToken in a base64 encoded format,
+// when using process.env.PRINTFUL_TOKEN, it does not work...
+export const printfulToken = 'OGloY2JnZ3YtaGwxbC0ybXptOnp4cGQtYzRldno2bm1hc3A4'
+export const stripeToken = ''
