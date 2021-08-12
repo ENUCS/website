fa846429b3df297d9e61116aefc53794a8bcf2a6
m <20924663+migbash@users.noreply.github.com>
2021-08-12 16:55:50 +0100

    adding the stripe config & 	oken values to the website

--- src/config/init-printful.ts
+++ src/config/init-printful.ts
@@ -1,4 +1,6 @@
-// exporting the printfulToken in a base64 encoded format,
+// exporting the printfulToken in a base64 encoded format;
 // when using process.env.PRINTFUL_TOKEN, it does not work...
 export const printfulToken = 'OGloY2JnZ3YtaGwxbC0ybXptOnp4cGQtYzRldno2bm1hc3A4'
-export const stripeToken = ''
+
+// stripte TEST Token;
+export const stripeToken = 'sk_test_51JKygiIyAXMjFyvNvskWWvXDYKYEVIXNlWP3vasaCtbgUqgrE99C4xN9NziCi6iAHpKxAIt88kahJj4ebKyt4kSv00YSqZQa8d'
