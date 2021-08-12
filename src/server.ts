7a07e4594c90a3c1694877f6dbb56e123f0cfdc9
m <20924663+migbash@users.noreply.github.com>
2021-08-12 16:57:24 +0100

    updating the server.ts file

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import * as sapper from "@sapper/server";
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
+// dealing with CORS - https://github.com/Rob--W/cors-anywhere/issues/301
+
 express()
 	// using .bodyPARSER to intercept data in the APPLICATION;
 	// https://stackoverflow.com/questions/66659450/getting-body-parser-is-deprecated-warning-in-vs-code-and-not-able-to-get-body-tr
