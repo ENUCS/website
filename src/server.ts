32cb139984b82835dc2461edf6073d8bbd8f02d8
m <20924663+migbash@users.noreply.github.com>
2021-08-12 04:37:57 +0100

    updating the package.json & -lock.json file for the correct use of the project equirements

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import sirv from "sirv";
+import express from "express";
+import compression from "compression";
+import * as sapper from "@sapper/server";
+var bodyParser = require('body-parser')
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+express()
+	// using .bodyPARSER to intercept data in the APPLICATION;
+	// https://stackoverflow.com/questions/66659450/getting-body-parser-is-deprecated-warning-in-vs-code-and-not-able-to-get-body-tr
+	.use(express.json({ limit: '50mb' }))
+	.use(express.urlencoded())
+
+    .use(
+        compression({ threshold: 0 }),
+        sirv("static", { dev }),
+        sapper.middleware()
+    )
+    .listen(PORT, () => {});
