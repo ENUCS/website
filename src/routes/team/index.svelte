4c1744e40d90197f22144779a78a8337a20de3bb
m <20924663+migbash@users.noreply.github.com>
2021-08-23 03:29:28 +0100

    updating the website /team page by removing the svg use and replacing with simple html-css for mobile

--- src/routes/team/index.svelte
+++ src/routes/team/index.svelte
@@ -1,72 +1,90 @@
-<!-- 
-~~~~~~~~~~~~
-	COMPONENT JS (w/ TS)
-~~~~~~~~~~~~
--->
+<!-- ===================
+	COMPONENT JS - BASIC 
+    [TypeScript Written]
+=================== -->
 
 <script lang="ts">
     import { onMount } from 'svelte';
-
-    import DesktopTeamView from '../../components/view/desktop/DesktopTeamView.svelte'
-    import MobileTeamView from '../../components/view/mobile/MobileTeamView.svelte'
-
-    let viewportDesktop: boolean;
-
-    onMount(async() => {
-        var wInit = document.documentElement.clientWidth;
-        if (wInit > 1024) {
-            viewportDesktop = true;
-        } else {
-            viewportDesktop = false;
-        }
-        window.addEventListener("resize", function() {
-            var w = document.documentElement.clientWidth;
-            if (w > 1024) {
-                viewportDesktop = true;
-            } else {
-                viewportDesktop = false;
-            }
-        });
-    })
-
-    // transition config, passed onto Nav & Footer also; (temporary)
-    let transition_time: number = 1000;
+    import TeamCard from './_TeamCard.svelte'
+    import { team_members_data } from '../../data/team-members'
 </script>
 
-<!-- 
-~~~~~~~~~~~~
-	SVELTE INJECTION TAGS
-~~~~~~~~~~~~
--->
+<!-- ===================
+	SVELTE INJECTION TAGS 
+=================== -->
 
 <svelte:head>
-    <title>ENUCS - Team</title>
+	<!-- 
+	Primary Meta Tags;
+	https://metatags.io/ -->
+	<title>ENUCS | Team</title>
+	<meta name="title" content="ENUCS | Team">
+	<meta name="description" content="Computing Society | ENUCS | Team">
+	<meta name="keywords" content="computing society, 
+        programming society, 
+        computing society university, 
+        society university, 
+        web development society, 
+        development society, 
+        edinburgh napier university,
+        enucs, 
+        enu,
+        university club,
+        programming community,
+        students,
+        student,
+        enu">
+	<meta name="author" content="Miguel Bacharov">
+	<!-- 
+	Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://www.enucs.org.uk/shop">
+	<meta property="og:title" content="ENUCS | Team">
+	<meta property="og:description" content="Computing Society | ENUCS | Team">
+    
+	<meta property="og:image" content="https://www.spacerealm.live/assets/img/logo-main.png">
+	<!-- 
+	Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://www.enucs.org.uk/shop">
+	<meta property="twitter:title" content="ENUCS | Team">
+	<meta property="twitter:description" content="Computing Society | ENUCS | Team">
+
+	<meta property="twitter:image" content="https://www.spacerealm.live/assets/img/logo-main.png">
 </svelte:head>
 
-<!-- 
-~~~~~~~~~~~~
+<!-- ===================
 	COMPONENT HTML
-~~~~~~~~~~~~
--->
+=================== -->
 
-{#if !viewportDesktop}
-    <div id='mobile-version'>
-        <MobileTeamView />
+<section>
+    <!-- 
+    ~~~~~~~~~~~~~~~
+    page-title -->
+    <h2 class='s-42 bold m-b-55'>
+        TEAM
+    </h2>
+    <!-- 
+    ~~~~~~~~~~~~~~~
+    team-members-card -->
+    <div id='team-member-grid'>
+        {#each team_members_data as item}
+            <TeamCard data={item} />
+        {/each}
     </div>
-{:else}
-    <div id='desktop-version'>
-        <DesktopTeamView />
-    </div>
-{/if}
+</section>
 
-<!-- 
-~~~~~~~~~~~~
+<!-- ===================
 	COMPONENT STYLE
-~~~~~~~~~~~~
--->
+=================== -->
 
 <style>
-    #desktop-version {
-        min-height: 100vh;
+    section {
+        display: grid;
+        justify-items: center;
+    }
+    #team-member-grid {
+        display: grid;
+        gap: 55px
     }
 </style>
