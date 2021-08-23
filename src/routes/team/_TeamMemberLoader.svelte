63312ad4b62d1088f2e4d61f02ddb8b9114e053b
m <20924663+migbash@users.noreply.github.com>
2021-08-23 03:30:03 +0100

    adding an svg loading module for 	eam-member component

---
+++ src/routes/team/_TeamMemberLoader.svelte
@@ -0,0 +1,10 @@
+<svelte:options namespace="svg" />
+<ContentLoader {...$$props}>
+    <rect x="19" width="96" height="93" rx="20" fill="#C4C4C4"/>
+    <rect y="110" width="135" height="17" rx="8.5" fill="#C4C4C4"/>
+    <rect y="133" width="135" height="17" rx="8.5" fill="#C4C4C4"/>
+</ContentLoader>
+
+<script>
+    import ContentLoader from 'svelte-content-loader'
+</script>
